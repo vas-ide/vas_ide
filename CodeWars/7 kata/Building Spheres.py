3387a07a0eb1b4cb0b6f2232e427d8076673ad44
VAS-PC-IDE <vas.ide2022@gmail.com>
2023-03-21 10:02:36 +0300

    CodeWars

--- CodeWars/7 kata/Building Spheres.py
+++ CodeWars/7 kata/Building Spheres.py
@@ -1,6 +1,4 @@
-
-
-
+import math
 
 
 class Sphere(object):
@@ -11,19 +9,22 @@ class Sphere(object):
 
     def get_radius(self):
         print(self.radius, "Check radius")
-        return self.radius, "Check radius"
+        return self.radius
     def get_mass(self):
         print(self.mass, "Check mass")
-        return self.mass, "Check mass"
+        return self.mass
     def get_volume(self):
-        # формула объемной сферы --- V=4/3πr**3
-        pass
+        # формула объемной сферы --- V=4/3*(πr**3)
+        print(round(4 / 3 * (math.pi * self.radius ** 3), 5), "Check volume")
+        return round(4 / 3 * (math.pi * self.radius ** 3), 5)
     def get_surface_area(self):
         # площадь поверхности сферы --- S=4πR**2
-        pass
+        print(round(4 * math.pi * self.radius ** 2, 5), "Check area")
+        return round(4 * math.pi * self.radius ** 2, 5)
     def get_density(self):
-        # плотность сферы
-        pass
+        # плотность сферы  --- p=m/V
+        print(round(self.mass / (4 / 3 * (math.pi * self.radius ** 3)), 5), "Check density")
+        return round(self.mass / (4 / 3 * (math.pi * self.radius ** 3)), 5)
 
 
 
@@ -34,9 +35,9 @@ ball = Sphere(2,50)
 
 ball.get_radius()                   #2, "Check radius"
 ball.get_mass()                     #50, "Check mass"
-# ball.get_volume()                   #33.51032, "Check volume"
-# ball.get_surface_area()             #50.26548, "Check area"
-# ball.get_density()                  #1.49208, "Check density"
+ball.get_volume()                   #33.51032, "Check volume"
+ball.get_surface_area()             #50.26548, "Check area"
+ball.get_density()                  #1.49208, "Check density"
 
 
 
