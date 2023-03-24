4499a7c2902400dea35041b582703b5bc7808a2e
vas-ide <vas.ide2022@gmail.com>
2023-03-24 20:28:12 +0300

    CodeWars

--- CodeWars/7 kata/Merge overlapping strings.py
+++ CodeWars/7 kata/Merge overlapping strings.py
@@ -4,10 +4,17 @@
 
 
 def merge_strings(first, second):
-    result = first
+    string_init = first
+    result = ""
+    couter = 0
     for _ in second:
-        if _ not in first:
+        couter += 1
+        if _ in first and result == "":
             result += _
+        elif _ in first:
+            result += _
+            if result not in first:
+                pass
     return result
 
 
