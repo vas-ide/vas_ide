a528d5154c739bd969d69a7bdb51d3318ea1a72f
vas-ide <vas.ide2022@gmail.com>
2023-03-25 09:35:57 +0300

    CodeWars --- req Class

--- CodeWars/6 kata/Array Deep Count.py
+++ CodeWars/6 kata/Array Deep Count.py
@@ -1,24 +1,33 @@
 
-def deep_count(a):
-    class ArrayDeepCount:
-        def __init__(self):
-            self.counter = 0
+# def deep_count(a):
+#     class ArrayDeepCount:
+#
+#         def __init__(self):
+#             self.counter = 0
+#
+#         def deep_count_add(self, a):
+#             for i in a:
+#                 self.counter += 1
+#                 if type(i) == list:
+#                     self.deep_count_add(i)
+#
+#         def answer(self):
+#             print(self.counter)
+#             return self.counter
+#
+#     ans = ArrayDeepCount()
+#     ans.deep_count_add(a)
+#     return ans.answer()
 
-
-        def deep_count_add(self, a):
-            for i in a:
-                self.counter += 1
-                if type(i) == list:
-                    self.deep_count_add(i)
-
-        def answer(self):
-            print(self.counter)
-            return self.counter
-
-    ans = ArrayDeepCount()
-    ans.deep_count_add(a)
-    ans.answer()
-
+# counter = []
+# def deep_count(a):
+#     global counter
+#     for i in a:
+#         counter.append(1)
+#         if type(i) == list:
+#             deep_count(i)
+#     print(len(counter))
+#     return len(counter)
 
 
 deep_count([])                          #0
