7feabe1c55d888b7cab8d20a2b79229fada3d433
vas-ide <vas.ide2022@gmail.com>
2023-03-25 11:40:29 +0300

    CodeWars --- req Class

--- CodeWars/6 kata/Array Deep Count.py
+++ CodeWars/6 kata/Array Deep Count.py
@@ -1,33 +1,25 @@
 
-# def deep_count(a):
-#     class ArrayDeepCount:
-#
-#         def __init__(self):
-#             self.counter = 0
-#
-#         def deep_count_add(self, a):
-#             for i in a:
-#                 self.counter += 1
-#                 if type(i) == list:
-#                     self.deep_count_add(i)
-#
-#         def answer(self):
-#             print(self.counter)
-#             return self.counter
-#
-#     ans = ArrayDeepCount()
-#     ans.deep_count_add(a)
-#     return ans.answer()
+def deep_count(a):
+    class ArrayDeepCount:
 
-# counter = []
-# def deep_count(a):
-#     global counter
-#     for i in a:
-#         counter.append(1)
-#         if type(i) == list:
-#             deep_count(i)
-#     print(len(counter))
-#     return len(counter)
+        def __init__(self):
+            self.counter = 0
+
+        def deep_count_add(self, a):
+            for i in a:
+                self.counter += 1
+                if type(i) == list:
+                    self.deep_count_add(i)
+
+        def answer(self):
+            print(self.counter)
+            return self.counter
+
+    ans = ArrayDeepCount()
+    ans.deep_count_add(a)
+    return ans.answer()
+
+
 
 
 deep_count([])                          #0
@@ -37,15 +29,4 @@ deep_count([1, 2, [3, 4, [5]]])         #7
 deep_count([[[[[[[[[]]]]]]]]])          #8
 
 
-
-# counter = []
-# def deep_count(a):
-#     global counter
-#     for i in a:
-#         counter.append(1)
-#         # print(len(counter))
-#         if type(i) == list:
-#             deep_count(i)
-#     print(len(counter))
-#     return len(counter)
 
