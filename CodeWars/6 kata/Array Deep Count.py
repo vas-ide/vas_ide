b3b2cfc808216522aef07e07d68f13e39ee6273d
vas-ide <vas.ide2022@gmail.com>
2023-03-25 08:19:21 +0300

    CodeWars --- req

---
+++ CodeWars/6 kata/Array Deep Count.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+def deep_count(a):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+deep_count([])                          #0
+deep_count([1, 2, 3])                   #3
+deep_count(["x", "y", ["z"]])           #4
+deep_count([1, 2, [3, 4, [5]]])         #7
+deep_count([[[[[[[[[]]]]]]]]])          #8
+
+
+
+
+
+
+
+
+
