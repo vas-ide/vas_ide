d213a871160d503f7e92d0e7536ec180f7fc9870
vas-ide <vas.ide2022@gmail.com>
2023-03-26 16:47:47 +0300

    Django you second part

--- Django/testyoutobe/youapp/views.py
+++ Django/testyoutobe/youapp/views.py
@@ -5,11 +5,20 @@ from youapp.models import Worker
 
 def index_page(request):
 
+    # new_worker = Worker(name="Иван", second_name="Болванов", salary=15500)
+    # new_worker.save()
+    # Worker.objects.create(name="Иван", second_name="Болванов", salary=15500)
+    # worker_to_chenge = Worker.objects.get(id=5)
+    # worker_to_chenge.second_name = "Дупgggлетов"
+    # worker_to_chenge.save()
+    # print(worker_to_chenge)
+
+    # Worker.objects.get(id=5).delete()
     all_workers = Worker.objects.all()
     print(all_workers)
 
-    workers_filtered = Worker.objects.filter(salary=15000)
-    print(workers_filtered)
+    # workers_filtered = Worker.objects.filter(salary=15000)
+    # print(workers_filtered)
     for i in all_workers:
         print(f"Фамилия-{i.second_name:>15},    Имя-{i.name:>10},    Оклад-{i.salary:>5}    ID{i.id}")
 
