6ad0933b1c8e554cd77f53147d32940fd14031bd
vas-ide <vas.ide2022@gmail.com>
2023-03-27 19:17:08 +0300

    Django add first app

--- Django/board/advertisement/views.py
+++ Django/board/advertisement/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def advertisement_list(request, *args, **kwargs):
-    return HttpResponse("<ul>"
-                        "<li>Delivery to clinic</li>"
-                        "<li>Опрос и первичный осмотр</li>"
-                        "<li>Диагностика и анализы</li>"
-                        "<li>Вмешательство или процедуры</li>"
-                        "<li>Вторичный анализ </li>"
-                        "<li>Delivery from clinic to home</li>"
-                        "</ul>"
-                        )
+def advertisement_list(request):
+    return render(request, "advertisement/advertisement_list.html", {})
 
 
 
-
-
+# def advertisement_list(request, *args, **kwargs):
+#     return HttpResponse("<ul>"
+#                         "<li>Delivery to clinic</li>"
+#                         "<li>Опрос и первичный осмотр</li>"
+#                         "<li>Диагностика и анализы</li>"
+#                         "<li>Вмешательство или процедуры</li>"
+#                         "<li>Вторичный анализ </li>"
+#                         "<li>Delivery from clinic to home</li>"
+#                         "</ul>"
+#                         )
 
 # def index_page(request):
 #     return render(request, "index.html")
