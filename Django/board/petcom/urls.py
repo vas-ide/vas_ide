abd9e79d9b8460979c12e554c0665168b6b5df75
vas-ide <vas.ide2022@gmail.com>
2023-03-28 22:24:40 +0300

    petcom innit

--- Django/board/petcom/urls.py
+++ Django/board/petcom/urls.py
@@ -1,2 +1,8 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path("about.html/", views.about_us, name="about"),
+    path("categories.html/", views.categories_us, name="categories"),
+    path("contacts/", views.contact_us, name="contact"),
+]
