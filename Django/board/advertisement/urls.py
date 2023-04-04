1636d1768ab20243e432137962c8d347617bc157
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-04 19:35:43 +0300

    Django contacts upd

--- Django/board/advertisement/urls.py
+++ Django/board/advertisement/urls.py
@@ -6,14 +6,14 @@ from . import views
 urlpatterns = [
     path("", views.Index.as_view()),
     path("advertisement/", views.Advertisement.as_view()),
-    path("about.html/", views.About.as_view()),
-    path("categories.html/", views.Categories.as_view()),
-    path("contacts.html/", views.Contacts.as_view()),
-    path("regions.html/", views.Regions.as_view()),
-    path("skillbox_main.html/", views.skillbox_main, name="skillbox_main"),
-    path("python_main.html/", views.python_main, name="python_main"),
-    path("pandas_main.html/", views.pandas_main, name="pandas_main"),
-    path("django_main.html/", views.django_main, name="django_main"),
-    path("git_main.html/", views.git_main, name="git_main"),
+    path("advertisement/about.html/", views.About.as_view()),
+    path("advertisement/categories.html/", views.Categories.as_view()),
+    path("advertisement/contacts.html/", views.Contacts.as_view()),
+    path("advertisement/regions.html/", views.Regions.as_view()),
+    path("advertisement/skillbox_main.html/", views.skillbox_main, name="skillbox_main"),
+    path("advertisement/python_main.html/", views.python_main, name="python_main"),
+    path("advertisement/pandas_main.html/", views.pandas_main, name="pandas_main"),
+    path("advertisement/django_main.html/", views.django_main, name="django_main"),
+    path("advertisement/git_main.html/", views.git_main, name="git_main"),
 
 ]
