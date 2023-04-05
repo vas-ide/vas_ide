54b4916542de8e20d62d7c4bf741d5cd2410bd22
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-05 21:32:14 +0300

    Django sinior start

--- Django/testyoutobe/youapp/models.py
+++ Django/testyoutobe/youapp/models.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-
-
+from products.models import Product
 
 
 class Worker(models.Model):
@@ -19,3 +17,4 @@ class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    products = models.ManyToManyField(Product)
