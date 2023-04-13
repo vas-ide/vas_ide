590bc38f716b993e79d0d46d51209573638626b6
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-13 12:43:04 +0300

    Django continue

--- Django/board/advertisement/models.py
+++ Django/board/advertisement/models.py
@@ -4,7 +4,15 @@ from django.db import models
 
 class Advertisement(models.Model):
     title = models.CharField(max_length=200)
-    pass
+    description = models.CharField(max_length=2000, default="", verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.FloatField(verbose_name="Цена", default=0)
+    views_count = models.IntegerField(verbose_name="Колличество просмотров", default=0)
+
+
+    def __str__(self):
+        return f"Название:{self.title}    Описание:{self.description}"
 
 
 
