a7fe32ee3ffe7ced40b82b441b23eb46f93ba2d9
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-13 12:15:36 +0300

    CodeWars Daily

---
+++ CodeWars/7 kata/All Star Code Challenge #22.py
@@ -0,0 +1,25 @@
+
+
+
+
+def to_time(seconds):
+    return ''
+
+
+to_time(3600)               #1 hour(s) and 0 minute(s)
+to_time(3601)               #1 hour(s) and 0 minute(s)
+to_time(3500)               #0 hour(s) and 58 minute(s)
+to_time(323500)             #89 hour(s) and 51 minute(s)
+to_time(0)                  #0 hour(s) and 0 minute(s)
+
+
+
+
+
+
+
+
+
+
+
+
