526a80d9f490a55b072a9b8327f449464aacb5bc
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-14 10:14:27 +0300

    Django SkillBox

--- Django/board/advertisement/models.py
+++ Django/board/advertisement/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class Advertisement(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="Название")
     description = models.CharField(max_length=2000, default="", verbose_name="Описание")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего обновления")
     price = models.FloatField(verbose_name="Цена", default=0)
     views_count = models.IntegerField(verbose_name="Колличество просмотров", default=0)
 
