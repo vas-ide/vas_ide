0739d54ebcf92f548b1e715d91dbfcc02d67a413
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-18 20:12:32 +0300

    Django Innit: Geometry

---
+++ Django/band/geometry/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    # path('<int:day_week>/', views.get_info_about_day_by_number),
+    # path('<str:day_week>/', views.get_info_about_day),
+]
