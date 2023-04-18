4e6f1982fa8997951a427196d1b52b1b71ffc77e
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-18 20:09:54 +0300

    Django continue urls and views

--- Django/band/week_days/views.py
+++ Django/band/week_days/views.py
@@ -4,21 +4,22 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.views.generic import TemplateView
 
 week_days_dict = {
-    "monday": "Сделать КТ, Погулять с ребенком.",
-    "tuesday": "Узнать результаты КТ, Поехать в Ростов.",
+    "monday": "Погулять с ребенком.",
+    "tuesday": "Погулять с ребенком.",
 }
 
 
 def get_info_about_day(request, day_week: str):
     description = week_days_dict.get(day_week)
     if description:
-        return HttpResponse(f"{description}")
+        return HttpResponse(f"{day_week.capitalize()} - {description}")
     else:
         return HttpResponseNotFound(f"Неизвестный адрес")
 
 
 def get_info_about_day_by_number(request, day_week: int):
-    if day_week:
-        return HttpResponse(f"Tooday is {day_week} week day!")
+    description = day_week
+    if 0 < description < 8:
+        return HttpResponse(f"Tooday is {description} week day!")
     else:
-        return HttpResponseNotFound(f"Неизвестный адрес")
+        return HttpResponseNotFound(f"В неделе всего 7 дней. Вы указали неправильный день - {day_week}")
