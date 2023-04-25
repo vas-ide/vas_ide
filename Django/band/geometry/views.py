9c1b602c0dae614a9c865a3295dd886ec0dfdbc6
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-25 17:54:28 +0300

    Django html HW add geometry

--- Django/band/geometry/views.py
+++ Django/band/geometry/views.py
@@ -20,15 +20,17 @@ def circle(request, radius: int):
 
 
 def get_rectangle_area(request, width: int, height: int):
-    redirect_url = reverse("rectangle-name", args=(width, height,))
-    return HttpResponseRedirect(redirect_url)
+    # redirect_url = reverse("rectangle-name", args=(width, height,))
+    # return HttpResponseRedirect(redirect_url)
+    return render(request, "geometry/rectangle.html", {})
 
 
 def get_square_area(request, width: int):
-    redirect_url = reverse("square-name", args=(width,))
-    return HttpResponseRedirect(redirect_url)
-
+    # redirect_url = reverse("square-name", args=(width,))
+    # return HttpResponseRedirect(redirect_url)
+    return render(request, "geometry/square.html", {})
 
 def get_circle_area(request, radius: int):
-    redirect_url = reverse("circle-name", args=(radius,))
-    return HttpResponseRedirect(redirect_url)
+    # redirect_url = reverse("circle-name", args=(radius,))
+    # return HttpResponseRedirect(redirect_url)
+    return render(request, "geometry/circle.html", {})
