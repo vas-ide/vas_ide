f01ae74116a3af7b9c7c01d8bfddfd0a62695878
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-25 17:56:46 +0300

    Django html HW add geometry

--- Django/band/geometry/views.py
+++ Django/band/geometry/views.py
@@ -8,29 +8,30 @@ import math
 
 
 def rectangle(request, width: int, height: int):
-    return HttpResponse(f"Площадь прямоугольника размером {width}x{height} равна {width * height}")
-
-
-def square(request, width: int):
-    return HttpResponse(f"Площадь квадрата размером {width}x{width} равна {width * width}")
-
-
-def circle(request, radius: int):
-    return HttpResponse(f"Площадь круга радиус {radius} равна {round((radius ** 2) * math.pi, 3)}")
-
-
-def get_rectangle_area(request, width: int, height: int):
-    # redirect_url = reverse("rectangle-name", args=(width, height,))
-    # return HttpResponseRedirect(redirect_url)
+    # return HttpResponse(f"Площадь прямоугольника размером {width}x{height} равна {width * height}")
     return render(request, "geometry/rectangle.html", {})
 
 
-def get_square_area(request, width: int):
-    # redirect_url = reverse("square-name", args=(width,))
-    # return HttpResponseRedirect(redirect_url)
+def square(request, width: int):
+    # return HttpResponse(f"Площадь квадрата размером {width}x{width} равна {width * width}")
     return render(request, "geometry/square.html", {})
 
-def get_circle_area(request, radius: int):
-    # redirect_url = reverse("circle-name", args=(radius,))
-    # return HttpResponseRedirect(redirect_url)
+def circle(request, radius: int):
+    # return HttpResponse(f"Площадь круга радиус {radius} равна {round((radius ** 2) * math.pi, 3)}")
     return render(request, "geometry/circle.html", {})
+
+def get_rectangle_area(request, width: int, height: int):
+    redirect_url = reverse("rectangle-name", args=(width, height,))
+    return HttpResponseRedirect(redirect_url)
+
+
+
+def get_square_area(request, width: int):
+    redirect_url = reverse("square-name", args=(width,))
+    return HttpResponseRedirect(redirect_url)
+
+
+def get_circle_area(request, radius: int):
+    redirect_url = reverse("circle-name", args=(radius,))
+    return HttpResponseRedirect(redirect_url)
+
