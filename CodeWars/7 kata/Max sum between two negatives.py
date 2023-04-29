19eb88c277f0ffde4136be20217ac1e2fe29d23b
Asus-Manjaro <vas.ide2022@gmail.com>
2023-04-29 16:28:58 +0300

    code

---
+++ CodeWars/7 kata/Max sum between two negatives.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+def max_sum_between_two_negatives(arr):
+    print(arr)
+
+max_sum_between_two_negatives([-1, 6, -2, 3, 5, -7])            # 8
+max_sum_between_two_negatives([5, -1, -2])                      # 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
