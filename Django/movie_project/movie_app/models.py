17ab1707003fe227ef98cd93bd54f13502f5c0f5
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-01 20:52:10 +0300

    Django movie and booke

--- Django/movie_project/movie_app/models.py
+++ Django/movie_project/movie_app/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
+    year = models.IntegerField(null=True)
     rating = models.IntegerField()
+    budget = models.IntegerField(default="0")
 
     def __str__(self):
-        return f"{self.name}-{self.rating}%"
+        return f"{self.name}-{self.year}-{self.rating}"
 
 
-
-
-
+# from movie_app.models import Movie
 
 
 
