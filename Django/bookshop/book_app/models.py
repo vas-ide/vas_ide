8e7ef005c7e8595cab93c859fab4e63a3c688c83
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-01 17:09:01 +0300

    Django movie and book

--- Django/bookshop/book_app/models.py
+++ Django/bookshop/book_app/models.py
@@ -6,6 +6,9 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.title}-{self.rating}"
+
 
 
 
--- Django/movie_project/movie_app/models.py
+++ Django/movie_project/movie_app/models.py
@@ -7,6 +7,9 @@ class Movie(models.Model):
     name = models.CharField(max_length=100)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.name}-{self.rating}%"
+
 
 
 
