cbfe7a5cd15ec19755a9999307ecba48a1e17642
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-02 13:31:20 +0300

    Django movie html innit

--- Django/movie_project/movie_app/views.py
+++ Django/movie_project/movie_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def show_all_movies(request):
+    content = {
+
+
+    }
+    return render(request, "movie_app/all_movies.html", content)
+
+
+
+
--- Django/movie_project/movie_project/urls.py
+++ Django/movie_project/movie_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("horoscope/", include("movie_app.urls")),
+    path("", include("movie_app.urls")),
 ]
