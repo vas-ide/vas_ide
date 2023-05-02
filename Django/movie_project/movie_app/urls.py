afe383d524afafad4117f65b79a4b371364c7f23
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-02 13:23:23 +0300

    Django movie html innit

---
+++ Django/movie_project/movie_app/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path("", views.index, name="horoscope-index"),
+    # path("type", views.type_zodiac),
+    # path("type/<str:type_zodiac>", views.zodiac_elements_type, name="zodiac_type"),
+    # path("<int:sign_zodiac>", views.get_info_about_sign_zodiac_by_number),
+    # path("<str:sign_zodiac>", views.get_info_about_sign_zodiac, name="horoscope-name"),
+]
