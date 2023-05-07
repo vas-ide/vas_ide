bb03d34a5c18e6333efdc4cbcc64c8685855b880
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-07 12:32:04 +0300

    Django slug innit

--- Django/movie_project/movie_app/models.py
+++ Django/movie_project/movie_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Movie(models.Model):
@@ -7,9 +8,15 @@ class Movie(models.Model):
     year = models.IntegerField(null=True)
     rating = models.IntegerField()
     budget = models.IntegerField(default="0")
+    slug = models.SlugField(default='', null=False)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Movie, self).save(*args, **kwargs)
 
     def get_url(self):
-        return reverse('one-movie', args=[self.id])
+        return reverse('one-movie', args=[self.slug ])
 
     def __str__(self):
         return f"{self.name}-{self.year}-{self.rating}"
--- Django/movie_project/movie_app/views.py
+++ Django/movie_project/movie_app/views.py
@@ -2,23 +2,22 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Movie
 
+
 def show_all_movies(request):
+    movies = Movie.objects.all()
+    for slug in movies:
+        slug.save()
+
     content = {
         'movies': Movie.objects.all(),
     }
     return render(request, "movie_app/all_movies.html", content)
 
 
-
-
-def show_one_movie(request, id_movie: int):
-    error404 = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie: str):
+    error404 = get_object_or_404(Movie, slug=slug_movie)
     content = {
         'movies': Movie.objects.all(),
-        'movie': Movie.objects.get(id=id_movie),
+        'movie': Movie.objects.get(slug=slug_movie),
     }
     return render(request, "movie_app/one_movie.html", content)
-
-
-
-
