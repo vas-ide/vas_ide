3b062ba2d30722bf8690f526bf06b1e3cd10b852
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-05-07 14:09:35 +0300

    Py - fix

---
+++ PY/Look up in the Dictionary/The Cheapest Flight.py
@@ -0,0 +1,36 @@
+
+
+
+
+def cheapest_flight(costs: list, a: str, b: str) -> int:
+    # your code here
+    return 0
+
+
+cheapest_flight([["A", "C", 100], ["A", "B", 20], ["B", "C", 50]], "A", "C")                    #== 70
+cheapest_flight([["A", "C", 100], ["A", "B", 20], ["B", "C", 50]], "C", "A")                    #== 70
+cheapest_flight( [
+            ["A", "C", 40],
+            ["A", "B", 20],
+            ["A", "D", 20],
+            ["B", "C", 50],
+            ["D", "C", 70],], "D", "C")                                                         #== 60
+cheapest_flight([["A", "C", 100], ["A", "B", 20], ["D", "F", 900]], "A", "F")                   #== 0
+cheapest_flight([["A", "B", 10], ["A", "C", 15], ["B", "D", 15], ["C", "D", 10]], "A", "D")     #== 25
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
