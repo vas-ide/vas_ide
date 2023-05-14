8f74e7470a6d5e851a326f92f9a6d7cbdd646396
Asus-Manjaro <vas.ide2022@gmail.com>
2023-05-14 15:59:38 +0300

    Django annotate in admin

--- Django/movie_project/movie_app/admin.py
+++ Django/movie_project/movie_app/admin.py
@@ -8,7 +8,17 @@ from .models import Movie
 # or
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ["name", "year", "rating"]
+    list_display = ["name", "year", "rating", "rating_status"]
     list_editable = ["year", "rating"]
     ordering = ["year", "-rating"]
     list_per_page = 5
+
+
+    def rating_status(self, movie: Movie):
+        if movie.rating <= 50:
+            return f"Fail."
+        elif movie.rating <= 70:
+            return f"One time film"
+        elif movie.rating <= 85:
+            return f"Good film"
+        return f"Best content"
