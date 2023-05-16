b7bcb824d7ac5bcfcc65fe82b2a6b582d4c6795a
Asus-Manjaro <vas.ide2022@gmail.com>
2023-05-16 19:29:22 +0300

    Django admin filter add filter

--- Django/movie_project/movie_app/admin.py
+++ Django/movie_project/movie_app/admin.py
@@ -7,6 +7,31 @@ from django.db.models import QuerySet
 #     list_display = ["name", "year", "rating"]
 # admin.site.register(Movie, MovieAdmin)
 # or
+
+class RatingFilter(admin.SimpleListFilter):
+    title = "Rating-filter"
+    parameter_name = "Rating-settings"
+    def lookups(self, request, model_admin):
+        return [
+            ("<40", "Low rating"),
+            ("from 40 to 59", "Middle rating"),
+            ("from 60 to 79", "Good rating"),
+            (">=80", "The best film"),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == "<40":
+            return queryset.filter(rating__lt=40)
+        elif self.value() == "from 40 to 59":
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        elif self.value() == "from 60 to 79":
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        elif self.value() == ">=80":
+            return queryset.filter(rating__gt=79)
+
+
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ["name", "year", "budget", "currency", "rating",   "rating_status"]
@@ -15,6 +40,7 @@ class MovieAdmin(admin.ModelAdmin):
     list_per_page = 15
     actions = ["set_dollars", "set_euro"]
     search_fields = ["name__istartswith", "rating"]
+    list_filter = ["name", "year", RatingFilter]
 
     @admin.display(ordering='rating', description='Оценка')
     def rating_status(self, movie: Movie):
@@ -42,4 +68,6 @@ class MovieAdmin(admin.ModelAdmin):
             f"Было обновлено {count_updated} записей.",
             messages.ERROR
         )
+
+
 
