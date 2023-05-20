916953b0c1c501d996c19c69e7454225760f830c
Asus-Manjaro <vas.ide2022@gmail.com>
2023-05-20 20:35:58 +0300

    Innit form and GET

--- Django/form_project/feedback/urls.py
+++ Django/form_project/feedback/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from . import views
-from .views import index
+from .views import index, hello
 from django.urls import path
 
 
 
 urlpatterns = [
     path('', index),
+    path('hello', hello),
 ]
--- Django/form_project/feedback/views.py
+++ Django/form_project/feedback/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, "feedback/feedback.html", context)
+
+
+def hello(request):
+    name = request.GET["name"]
+    surname = request.GET["surname"]
+    login = request.GET["login"]
+    passwd = request.GET["passwd"]
+    feedback = request.GET["feedback"]
+    context = {
+        "name": name,
+        "surname": surname,
+        "login": login,
+        "passwd": passwd,
+        "feedback": feedback,
+    }
+    return render(request, "feedback/feedback.html", context)
