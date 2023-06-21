7367b93a96016b3e042e6368aa8c14958f645e7c
Asus-Manjaro <vas.ide2022@gmail.com>
2023-06-21 15:39:52 +0300

    python PY continue calculator fix init analiz part II restart

--- PY/SCIENTIFIC EXPEDITION/Calculator - II.py
+++ PY/SCIENTIFIC EXPEDITION/Calculator - II.py
@@ -81,18 +81,21 @@ def calculator(log: str) -> str:
                         if len(self.inf_init_analiz) == 1:
                             self.result_arg = int(self.inf_init_analiz[0])
                         elif len(self.inf_init_analiz) == 2:
-                            if len(self.inf_init_analiz[1]) > 1 and self.inf_init_analiz[1][0] == self.symbol_lst[0]:
-                                counter = 1
-                                while counter <= len(self.inf_init_analiz[1]):
-                                    self.result_arg += int(self.inf_init_analiz[0])
-                                    counter += 1
-                                break
-                            elif len(self.inf_init_analiz[1]) > 1 and self.inf_init_analiz[1][0] == self.symbol_lst[1]:
-                                counter = 1
-                                while counter <= len(self.inf_init_analiz[1]):
-                                    self.result_arg -= int(self.inf_init_analiz[0])
-                                    counter += 1
-                                break
+                            if self.inf_init_analiz[1][0] == self.symbol_lst[0]:
+                                self.first_arg = self.inf_init_analiz[0]
+                                self.result_arg = int(self.inf_init_analiz[0])
+                                for i in self.inf_init_analiz[1][1:]:
+                                    if i != self.symbol_lst[2]:
+                                        break
+                                    else:
+                                        self.result_arg += int(self.first_arg)
+                            elif self.inf_init_analiz[1][0] == self.symbol_lst[1]:
+                                self.result_arg = 0
+                            #     counter = 1
+                            #     while counter <= len(self.inf_init_analiz[1]):
+                            #         self.result_arg -= int(self.inf_init_analiz[0])
+                            #         counter += 1
+                            #     break
                             else:
                                 self.result_arg = int(self.inf_init_analiz[0])
 
@@ -115,6 +118,39 @@ def calculator(log: str) -> str:
                                     self.result_arg = (self.inf_init_analiz[_ + 1])
 
 
+                            elif len(__) > 1 and len(self.inf_init_analiz) >= (_ + 2):
+                                if __[0] == self.symbol_lst[0]:
+                                    self.second_arg = self.inf_init_analiz[_ - 1]
+                                    for i in __[1:]:
+                                        if i != self.symbol_lst[2]:
+                                            break
+                                        else:
+                                            self.result_arg += int(self.second_arg)
+                                elif __[0] == self.symbol_lst[1]:
+                                    self.result_arg = 0
+                                    # self.second_arg = self.inf_init_analiz[_ - 1]
+                                    # for i in __[1:]:
+                                    #     if i != self.symbol_lst[2]:
+                                    #         break
+                                    #     else:
+                                    #         self.result_arg -= int(self.second_arg)
+                                elif __[0] == self.symbol_lst[2]:
+                                    self.second_arg = self.inf_init_analiz[_ - 1]
+                                    if self.symbol_arg_add == self.symbol_lst[1]:
+                                        for i in __[1:]:
+                                            if i != self.symbol_lst[2]:
+                                                break
+                                            else:
+                                                self.result_arg -= int(self.second_arg)
+                                    else:
+                                        for i in __[1:]:
+                                            if i != self.symbol_lst[2]:
+                                                break
+                                            else:
+                                                self.result_arg += int(self.second_arg)
+                                        break
+
+
                             elif len(__) > 1 and len(self.inf_init_analiz) == (_ + 1):
                                 if __[0] == self.symbol_lst[0]:
                                     self.second_arg = self.inf_init_analiz[_ - 1]
@@ -124,12 +160,13 @@ def calculator(log: str) -> str:
                                         else:
                                             self.result_arg += int(self.second_arg)
                                 elif __[0] == self.symbol_lst[1]:
-                                    self.second_arg = self.inf_init_analiz[_ - 1]
-                                    for i in __[1:]:
-                                        if i != self.symbol_lst[2]:
-                                            break
-                                        else:
-                                            self.result_arg -= int(self.second_arg)
+                                    self.result_arg = 0
+                                    # self.second_arg = self.inf_init_analiz[_ - 1]
+                                    # for i in __[1:]:
+                                    #     if i != self.symbol_lst[2]:
+                                    #         break
+                                    #     else:
+                                    #         self.result_arg -= int(self.second_arg)
                                 elif __[0] == self.symbol_lst[2]:
                                     self.second_arg = self.inf_init_analiz[_ - 1]
                                     if self.symbol_arg_add == self.symbol_lst[1]:
@@ -152,25 +189,32 @@ def calculator(log: str) -> str:
             # print(cac.inf)
             print(cac.inf_init_analiz)
             print(cac.result_arg)
-            # return f"{cac.result_arg}"
+            return f"{cac.result_arg}"
 
         case _:
             print(f"Непредвиденная ошбка нуэен дополнительный анализ.")
 
 
-
-calculator("3+=")
+# calculator('78-=')
+# #'0'
+# calculator('3+2-=')
+# #'0'
+# calculator("3+=")
 # "6"
-calculator("3+2==")
+# calculator("3+2==")
 # # "7"
 # calculator("3+-2=")
 # # "1"
-calculator("-=-+3-++--+-2=-")
+# calculator("-=-+3-++--+-2=-")
 # # # "1"
 # calculator("000000")
 # # # "0"
 # calculator("0000123")
 # # # "123"
+calculator('2+3=+7=')
+#'12'
+# calculator('+-=12=')
+#'12'
 # calculator("12")
 # # # "12"
 # calculator("+12")
