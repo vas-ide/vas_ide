6d9d5fdfb1782232cc8c1ea162bf1c5974d98fcd
VAS-IDE-Win-11-PC <vas.ide2022@gmail.com>
2023-07-24 11:05:24 +0300

    Code-Wars - 6-kata

---
+++ CodeWars/6 kata/Counting Duplicates.py
@@ -0,0 +1,13 @@
+
+
+
+
+def duplicate_count(text):
+    pass
+
+duplicate_count("")#0
+duplicate_count("abcde")#0
+duplicate_count("abcdeaa")#1
+duplicate_count("abcdeaB")#2
+duplicate_count("Indivisibilities")#2
+
