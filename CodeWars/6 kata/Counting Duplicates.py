6ee6e49678fd6760e9a6f3ddc912f57be6a29928
VAS-IDE-Win-11-PC <vas.ide2022@gmail.com>
2023-07-24 11:19:14 +0300

    Code-Wars - 6-kata

--- CodeWars/6 kata/Counting Duplicates.py
+++ CodeWars/6 kata/Counting Duplicates.py
@@ -3,7 +3,19 @@
 
 
 def duplicate_count(text):
-    pass
+    data_dict = {}
+    counter_dub = 0
+    for i in text.lower():
+        if i in data_dict:
+            data_dict[i] += 1
+        else:
+            data_dict[i] = 1
+    for i in data_dict.values():
+        if i > 1:
+            counter_dub += 1
+
+    print(data_dict, counter_dub)
+    return counter_dub
 
 duplicate_count("")#0
 duplicate_count("abcde")#0
