38e6a30ac11e517603f90a10c2c6e627c11be512
Asus-Manjaro <vas.ide2022@gmail.com>
2023-08-31 16:39:03 +0300

    py - Popular Words.py

---
+++ Coding/PY/Look up in the Dictionary/Popular Words.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+def popular_words(text: str, words: list) -> dict:
+    upd_text = text.replace('\n', ' ').lower().split(' ')
+    rezult_dict = dict.fromkeys(words, 0)
+    for i in upd_text:
+        if i in words:
+            if i in rezult_dict:
+                rezult_dict[i] += 1
+    print(rezult_dict)
+    return rezult_dict
+
+
+
+
+popular_words(
+    "\nWhen I was One\nI had just begun\nWhen I was Two\nI was nearly new\n",
+    ["i", "was", "three", "near"],
+)
+
+#{"i": 4, "was": 3, "three": 0, "near": 0}
+
