d6c83531d4338b63bf4a5e8f64f013e0663e19d7
vas-ide <vas.ide2022@gmail.com>
2023-09-08 16:57:56 +0300

    archive regex

--- Projects/selfedu/siteinformation/workshift/urls.py
+++ Projects/selfedu/siteinformation/workshift/urls.py
@@ -1,16 +1,20 @@
 
-
-
+from
+from django.urls import path, re_path, register_converter
 from . import views
-from django.urls import path, re_path
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, name='year4')
 
 
 urlpatterns = [
     path('', views.all_information, name='all-info'),
     path('employee/<int:employee_id>', views.employee_information, name='employee-info_id'),
     path('employee/<slug:employee_slug>', views.employee_information_slug, name='employee-info_slug'),
-    re_path(r'archive/(?P<year>[0-9]{4})/', views.archive_year, name="archive_year")
-    # path('employee', views.employee_information, name='employee-info'),
+    # re_path(r'archive/(?P<year>[0-9]{4})/', views.archive_year, name="archive_year"),
+    path('archive/(<year4>:year/', views.archive_year, name="archive_year"),
+
 ]
 
 
