ca21a7fde13895f2bec7e13b54c31bbaa14c1269
vas-ide <vas.ide2022@gmail.com>
2023-09-08 16:30:43 +0300

    id & slug

--- Projects/selfedu/siteinformation/workshift/urls.py
+++ Projects/selfedu/siteinformation/workshift/urls.py
@@ -7,7 +7,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.all_information, name='all-info'),
-    path('employee', views.employee_information, name='employee-info'),
+    path('employee/<int:employee_id>', views.employee_information, name='employee-info_id'),
+    path('employee/<slug:employee_slug>', views.employee_information_slug, name='employee-info_slug'),
+    # path('employee', views.employee_information, name='employee-info'),
 ]
 
 
--- Projects/selfedu/siteinformation/workshift/views.py
+++ Projects/selfedu/siteinformation/workshift/views.py
@@ -11,10 +11,30 @@ def all_information(request):
 
 
 # employee
-def employee_information(request):
+def employee_information(request, employee_id):
 
 
     content = {
-
+        'employee_id': employee_id
     }
     return render(request, "workshift/employee_info.html", content)
+
+def employee_information_slug(request, employee_slug):
+
+
+    content = {
+        'employee_slug': employee_slug
+    }
+    return render(request, "workshift/employee_info_slug.html", content)
+
+
+
+
+
+
+
+
+
+
+
+
