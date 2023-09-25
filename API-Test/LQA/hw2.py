a4d7f8feec6408453a80b3ff56c83a27af606fd3
VAS-IDE-Win-11-PC <vas.ide2022@gmail.com>
2023-09-25 15:23:35 +0300

    api

---
+++ API-Test/LQA/hw2.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+# json_text = "{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}"
+
+
+
+json_text2 = {"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}
+
+
+
+
+
+
+
+
+
+
--- API-Test/LQA/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get('https://playground.learnqa.ru/api/hello')
-
-print(response)
-print(response.text)
