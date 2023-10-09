d329de7b6b416f8aa65e4ca45ffae95131835649
vas-ide-Manjaro <vas.ide2022@gmail.com>
2023-10-09 22:00:23 +0300

    CALC II in Progres

--- Coding/PY/SCIENTIFIC EXPEDITION/Calculator - II.py
+++ Coding/PY/SCIENTIFIC EXPEDITION/Calculator - II.py
@@ -7,6 +7,7 @@ class Calculator:
         self.before_arg = ""
         self.after_arg = ""
         self.result_arg = 0
+        self.upd_simbol = ""
 
     def run(self):
         try:
@@ -37,9 +38,28 @@ class Calculator:
             elif len(not_digit_str) > 0:
                 self.string_lst.append(not_digit_str)
 
+            for i, j in enumerate(self.string_lst):
+                if type(j) == str and len(j) > 1:
+                    if i != len(self.string_lst) - 1:
+                        self.string_lst[i] = j[-1]
+                    else:
+                        for k in j:
+                            if len(self.upd_simbol) < 1:
+                                self.upd_simbol = k
+                            else:
+                                if len(self.upd_simbol) > 0 and k != self.simbol_lst[2]:
+                                    self.upd_simbol = k
+                                else:
+                                    self.upd_simbol += k
+                        self.string_lst[i] = self.upd_simbol
+
+            print(self.string_lst)
+
             if len(self.string_lst) == 2 and self.string_lst[1] in self.simbol_lst:
                 self.result_arg = f"{self.string_lst[0]}"
                 return self.result_arg
+            elif len(self.string_lst) == 2 and len(self.string_lst[1]) > 1:
+                self.result_arg = f"{self.string_lst[0]}{self.string_lst[1][0]}{self.string_lst[0]}{self.string_lst[1][1:]}"
             elif len(self.string_lst) > 2 and self.string_lst[-1] not in self.simbol_lst:
                 self.result_arg = f"{self.string_lst[-1]}"
                 return self.result_arg
@@ -61,31 +81,40 @@ def calculator(log: str) -> str:
     print(calc.result_arg, type(calc.result_arg))
     return calc.result_arg
 
-
-calculator('-5-10+15')
-calculator('000005+003') == '3'
-calculator("-25")
-calculator("000000")
-# # "0"
-calculator("0000123")
-# # "123"
-calculator("12")
-# # "12"
-calculator("+12")
-# # "12"
-calculator("")
-# # "0"
-calculator("1+2")
-# # "2"
-calculator("2+")
+calculator("3+=")
+# "6"
+calculator("3+2==")
+# "7"
+calculator("4-1==")
 # "2"
-calculator("1+2=")
-# # "3"
-calculator("1+2-")
-# # "3"
-calculator("1+2=2")
+calculator("3+-2=")
+# "1"
+calculator("-=-+3-++--+-2=-")
+# "1"
+calculator('-5-10+15')
+# calculator('000005+003') == '3'
+# calculator("-25")
+# calculator("000000")
+# # # "0"
+# calculator("0000123")
+# # # "123"
+# calculator("12")
+# # # "12"
+# calculator("+12")
+# # # "12"
+# calculator("")
+# # # "0"
+# calculator("1+2")
+# # # "2"
+# calculator("2+")
 # # "2"
-calculator("=5=10=15")
-# "15"
+# calculator("1+2=")
+# # # "3"
+# calculator("1+2-")
+# # # "3"
+# calculator("1+2=2")
+# # # "2"
+# calculator("=5=10=15")
+# # "15"
 
 
