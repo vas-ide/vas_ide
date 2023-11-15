5349900418e00e73e45c1516c9b287af5d1b6c63
VAS-IDE <vas.ide2022@gmail.com>
2023-11-15 19:48:06 +0300

    Finished Flask

--- Projects/dj_old/egorof/form_project/feedback/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-
-class FeedbackForm(forms.Form):
-    name = forms.CharField()
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={"rows": 5, "cols": 50}))
