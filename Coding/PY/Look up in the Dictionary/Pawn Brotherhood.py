64f49cc22264c9e7a5debba159e278ffdd81d191
VAS-IDE-Win-11-PC <vas.ide2022@gmail.com>
2024-01-29 21:30:26 +0300

    Checkio

--- Coding/PY/Look up in the Dictionary/Pawn Brotherhood.py
+++ Coding/PY/Look up in the Dictionary/Pawn Brotherhood.py
@@ -1,12 +1,18 @@
 def safe_pawns(pawns: set) -> int:
-    # add your code here
-    return 0
+    line = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    column = [1, 2, 3, 4, 5, 6, 7, 8]
+    counter = 0
+    for item in pawns:
+
+        pass
 
 
-print("Example:")
-print(safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}))
 
-assert safe_pawns({"f4", "d4", "e3", "b4", "g5", "d2", "c3"}) == 6
-assert safe_pawns({"f4", "c4", "b4", "e4", "g4", "d4", "e5"}) == 1
 
-print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})
+
+# assert safe_pawns({"f4", "d4", "e3", "b4", "g5", "d2", "c3"}) == 6
+# assert safe_pawns({"f4", "c4", "b4", "e4", "g4", "d4", "e5"}) == 1
+
+
