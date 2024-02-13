7f2e9ae927e8874a771c942913a3880e26015c54
VAS <vas.ide2022@gmail.com>
2024-02-13 10:21:50 +0300

    Py

--- Coding/PY/Seven Segment.py
+++ Coding/PY/Seven Segment.py
@@ -16,26 +16,31 @@ class Display:
                 self.analiz_left.append(item)
             else:
                 self.analiz_right.append(item)
-        available_left = "".join(sorted(self.analiz_left))
-        available_right = "".join(sorted(self.analiz_right))
-        for key, value in self.digits_dict.items():
-            if key.upper() in available_left:
-                self.left_digits.append(value)
-            if key in available_right:
-                self.right_digits.append(value)
-
-    def generate(self):
-        result_lst = []
-        for item in self.left_digits:
-            for item_add in self.right_digits:
+        for key, value in self.digits_dict:
+            available_left = "".join(sorted(self.analiz_left))
+            available_right = "".join(sorted(self.analiz_right))
+            for item in key:
                 pass
 
+
+        # if key.upper() in available_left:
+        #     self.left_digits.append(value)
+        # if key in available_right:
+        #     self.right_digits.append(value)
+
+    def generate(self):
+        result = set()
+        for item in self.left_digits:
+            for item_add in self.right_digits:
+                digit = int(f"{item}{item_add}")
+                result.add(digit)
+        # print(result)
 
     def run(self):
         self.analiz()
         self.generate()
-        # print(sorted(self.analiz_left), '\n', sorted(self.analiz_right))
-        # print(self.left_digits, "\n", self.right_digits)
+        print(sorted(self.analiz_left), '\n', sorted(self.analiz_right))
+        print(self.left_digits, "\n", self.right_digits)
 
 
 def seven_segment(lit_seg, broken_seg):
@@ -54,3 +59,25 @@ seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'e'})
 # assert seven_segment({'B', 'C', 'b', 'c'}, {'A'}) == 2, '11, 71'
 # assert seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'e'}) == 6, '15, 16, 35, 36, 75, 76'
 # assert seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'F', 'b', 'e'}) == 20, '15...98'
+
+
+# a = ['a', 'c', 'd', 'e', 'f', 'g']
+# b = ['a', 'c', 'd', 'f', 'g']
+# a = "acdefg"
+# b = "acdfg"
+#
+# if b in a:
+#     print(f"yes")
+# else:
+#     print(f"errors")
+#
+#
+#
+# a = ['a', 'c', 'd', 'e', 'f', 'g']
+# b = ['a', 'c', 'd', 'f', 'g']
+#
+#
+# if b in a:
+#     print(f"yes")
+# else:
+#     print(f"errors")
