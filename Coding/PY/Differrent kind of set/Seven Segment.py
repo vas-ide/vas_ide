d556ee189032b0dfef8e5a29d9a4058c8e595cd9
VAS <vas.ide2022@gmail.com>
2024-02-13 21:12:24 +0300

    Py

--- Coding/PY/Differrent kind of set/Seven Segment.py
+++ Coding/PY/Differrent kind of set/Seven Segment.py
@@ -9,6 +9,7 @@ class Display:
         self.right_digits = []
         self.digits_dict = {"bc": 1, "abdeg": 2, "abcdg": 3, "bcfg": 4, "acdfg": 5, "acdefg": 6, "abc": 7, "abcdefg": 8,
                             "abcdfg": 9, "abcdef": 0}
+        self.total = 0
 
     def analiz(self):
         for item in self.available.union(self.broken):
@@ -16,17 +17,21 @@ class Display:
                 self.analiz_left.append(item)
             else:
                 self.analiz_right.append(item)
-        for key, value in self.digits_dict:
-            available_left = "".join(sorted(self.analiz_left))
-            available_right = "".join(sorted(self.analiz_right))
-            for item in key:
-                pass
-
-
-        # if key.upper() in available_left:
-        #     self.left_digits.append(value)
-        # if key in available_right:
-        #     self.right_digits.append(value)
+        # for key, value in self.digits_dict.items():
+        #     available_left = "".join(sorted(self.analiz_left))
+        #     available_right = "".join(sorted(self.analiz_right))
+        #     str_digit = ""
+        #     for item in key:
+        #         if item.upper() in available_left:
+        #             str_digit += item
+        #     if key == str_digit:
+        #         self.left_digits.append(value)
+        #     str_digit = ""
+        #     for item in key:
+        #         if item in available_right:
+        #             str_digit += item
+        #     if key == str_digit:
+        #         self.right_digits.append(value)
 
     def generate(self):
         result = set()
@@ -34,13 +39,13 @@ class Display:
             for item_add in self.right_digits:
                 digit = int(f"{item}{item_add}")
                 result.add(digit)
-        # print(result)
+        print(len(result), result)
+        self.total = len(result)
 
     def run(self):
         self.analiz()
-        self.generate()
-        print(sorted(self.analiz_left), '\n', sorted(self.analiz_right))
-        print(self.left_digits, "\n", self.right_digits)
+        # self.generate()
+        # return self.total
 
 
 def seven_segment(lit_seg, broken_seg):
@@ -52,32 +57,14 @@ seven_segment({'B', 'C', 'b', 'c'}, {'A'})
 # == 2, '11, 71'
 seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'e'})
 # # == 6, '15, 16, 35, 36, 75, 76'
-# seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'F', 'b', 'e'})
+seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'F', 'b', 'e'})
 # # == 20, '15...98'
+seven_segment({'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'}, {'G', 'g'})
+# # == 4   "0, 8, 80, 88"
+
 
 
 # assert seven_segment({'B', 'C', 'b', 'c'}, {'A'}) == 2, '11, 71'
 # assert seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'e'}) == 6, '15, 16, 35, 36, 75, 76'
 # assert seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'F', 'b', 'e'}) == 20, '15...98'
 
-
-# a = ['a', 'c', 'd', 'e', 'f', 'g']
-# b = ['a', 'c', 'd', 'f', 'g']
-# a = "acdefg"
-# b = "acdfg"
-#
-# if b in a:
-#     print(f"yes")
-# else:
-#     print(f"errors")
-#
-#
-#
-# a = ['a', 'c', 'd', 'e', 'f', 'g']
-# b = ['a', 'c', 'd', 'f', 'g']
-#
-#
-# if b in a:
-#     print(f"yes")
-# else:
-#     print(f"errors")
