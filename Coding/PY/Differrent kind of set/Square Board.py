19c47338fae7cfd8e6e9b1a14e0642e38da35c63
VAS <vas.ide2022@gmail.com>
2024-02-21 15:10:37 +0300

    Py

---
+++ Coding/PY/Differrent kind of set/Square Board.py
@@ -0,0 +1,11 @@
+from typing import Tuple
+Coordinate = Tuple[int, int]
+
+
+def square_board(side: int, token: int, steps: int) -> Coordinate:
+    return (0, 0)
+
+
+# assert square_board(4, 1, 4) == (1, 0)
+# assert square_board(6, 2, -3) == (4, 5)
+
