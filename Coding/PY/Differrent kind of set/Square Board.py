ab2fdd881cd7893de1765ba93e5669bac1c73d28
VAS <vas.ide2022@gmail.com>
2024-02-21 15:22:59 +0300

    Py

--- Coding/PY/Differrent kind of set/Square Board.py
+++ Coding/PY/Differrent kind of set/Square Board.py
@@ -3,9 +3,14 @@ Coordinate = Tuple[int, int]
 
 
 def square_board(side: int, token: int, steps: int) -> Coordinate:
-    return (0, 0)
+    game_lst = [item for item in range(side * 4 - 4)]
+    possition = game_lst[token + steps]
+    row = None
+    line = None
+    print(game_lst, len(game_lst), possition)
 
-
+square_board(4, 1, 4)
+square_board(6, 2, -3)
 # assert square_board(4, 1, 4) == (1, 0)
 # assert square_board(6, 2, -3) == (4, 5)
 
