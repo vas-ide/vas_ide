51120d87c8882ec5575f215d8d981e40e3e48bea
VAS-IDE <vas.ide2022@gmail.com>
2024-05-07 16:04:15 +0300

    skillbox_data_packages

--- Coding/PY/Differrent kind of set/Berserk Rook.py
+++ Coding/PY/Differrent kind of set/Berserk Rook.py
@@ -1,13 +1,26 @@
-
-
-
 def berserk_rook(berserker, enemies):
-    return 0
+    enemies = list(enemies)
+    field_symbol_lst = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    field_numb_lst = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    lst_total_results = []
+    lst_results = []
+    wheel = True
+    while wheel == True:
+        for item in enemies:
+            if item[0] == berserker[0]:
+                marker = item
+            elif item[1] == berserker[1]:
+                marker = item
+
+            else:
+                break
 
 
 
+berserk_rook('d3', {'d6', 'b6', 'c8', 'g4', 'b8', 'g6'})
+berserk_rook('a2', {'f6', 'f2', 'a6', 'f8', 'h8', 'h6'})
+berserk_rook('a2', {'f6', 'f8', 'f2', 'a6', 'h6'})
 
-assert berserk_rook('d3', {'d6', 'b6', 'c8', 'g4', 'b8', 'g6'}) == 5, "one path"
-assert berserk_rook('a2', {'f6', 'f2', 'a6', 'f8', 'h8', 'h6'}) == 6, "several paths"
-assert berserk_rook('a2', {'f6', 'f8', 'f2', 'a6', 'h6'}) == 4, "Don't jump through"
-
+# assert berserk_rook('d3', {'d6', 'b6', 'c8', 'g4', 'b8', 'g6'}) == 5, "one path"
+# assert berserk_rook('a2', {'f6', 'f2', 'a6', 'f8', 'h8', 'h6'}) == 6, "several paths"
+# assert berserk_rook('a2', {'f6', 'f8', 'f2', 'a6', 'h6'}) == 4, "Don't jump through"
